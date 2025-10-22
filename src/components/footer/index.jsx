2b3bddf6c8c31c9e0cbe220c219556ad23e6f3c6
Kshitiz Sharma <kshitiz.strt@gmail.com>
2025-10-22 17:36:54 +0530

    update: footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,12 @@
 function Footer() {
+    const currentYear = new Date().getFullYear();
+
     return (
-        <footer className="w-full py-4 text-center text-gray-400 text-sm"
+        <footer
+            className="w-full py-4 text-center text-gray-400 text-sm"
             style={{ backgroundColor: 'rgba(203, 203, 203, 0.1)' }}
         >
-            Data powered by{' '}
-            <a
-                href="https://pokeapi.co/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline hover:text-gray-600 transition"
-            >
-                PokéAPI
-            </a>
+            Copyright &copy; {currentYear} Pokédex.
         </footer>
     );
 }
