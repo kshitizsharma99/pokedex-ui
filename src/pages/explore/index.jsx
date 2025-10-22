dc8cf7037ce6a3cd57ff27007680d9b9193f7a97
Kshitiz Sharma <kshitiz.strt@gmail.com>
2025-10-22 23:35:21 +0530

    update: explorePage const navigate = useNavigate();

--- src/pages/explore/index.jsx
+++ src/pages/explore/index.jsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import usePokemonStore from "../../store/pokemonStore";
 import PokemonOverview from "../../components/pokemonOverview";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 function Explore() {
     const selectedPokemon = usePokemonStore(state => state.selectedPokemon);
     const fetchMorePokemonDetails = usePokemonStore(state => state.fetchMorePokemonDetails);
+    const navigate = useNavigate();
+
 
     useEffect(() => {
         if (selectedPokemon) {
@@ -12,7 +16,20 @@ function Explore() {
         }
     }, [selectedPokemon]);
 
-    if (!selectedPokemon) return <p>Loading Pokémon...</p>;
+    if (!selectedPokemon) return (
+        <div>
+
+            <div className=" rounded-lg bg-transparent sahdow-xl py-2 px-5">
+                <div className="my-1 px-7">
+                    <Button className="rounded-full w-36 !text-base !bg-transparent !border-none !shadow-none !text-black hover:!bg-transparent hover:!text-black focus:!outline-none focus:!shadow-none"
+                        type="primary"
+                        icon={<span className="text-xl mr-0.5 ">←</span>}
+                        onClick={() => { navigate("/pokedex") }}>No Pokemon Selected. Return To Pokedex</Button>
+
+                </div>
+            </div>
+        </div>
+    );
 
     return <PokemonOverview pokemon={selectedPokemon} />;
 }
